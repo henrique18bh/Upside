eb374ac91f3bf64f199c1a6d5c7d755bcedeace2
Henrique Burle <henriquebm@ciandt.com>
2018-01-06 19:37:56 -0200

    Questão 1 e 2

---
+++ Prova/NumerosPrimos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPrimos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (IsPrimo(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static bool IsPrimo(int numero)
+        {
+            if (numero == 1)
+            {
+                return false;
+            }
+            if (numero == 2)
+            {
+                return true;
+            }
+            if (numero % 2 == 0)
+            {
+                return false;
+            }
+            var raiz = Math.Sqrt(numero);
+            var fronteira = (int)Math.Floor(raiz);
+            for (int i = 3; i <= fronteira; i += 2)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Prova/VetorOrdenado/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorOrdenado
+{
+    public class Program
+    {
+
+        private static int[] Vetor = { 1, 2, 30 };
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (ValidarNumero(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static bool ValidarNumero(int numero)
+        {
+            return Vetor.Any(x => x.Equals(numero));
+        }
+    }
+}
